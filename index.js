52ceb553747aaceaf4f5b6698d150860eff8e93e
williamg1750 <william.guan12@gmail.com>
2021-06-15 17:20:36 -0400

    bcrypt added

--- index.js
+++ index.js
@@ -1,12 +1,38 @@
 const express = require('express');
 const app = express();
 const User = require('./models/user');
+const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+
+mongoose.connect('mongodb://localhost:27017/authDemo', {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useUnifiedTopology: true,
+  useFindAndModify: false,
+});
 
 app.set('view engine', 'ejs');
 app.set('views', 'views');
 
+app.use(express.urlencoded({ extended: true }));
+
+app.get('/', (req, res) => {
+  res.send('Home page');
+});
+
 app.get('/register', (req, res) => {
   res.render('register');
+});
+
+app.post('/register', async (req, res) => {
+  const { username, password } = req.body;
+  const hash = await bcrypt.hash(password, 12);
+  const user = new User({
+    username,
+    password: hash,
+  });
+  await user.save();
+  res.redirect('/');
 });
 
 app.get('/secret', (req, res) => {
