0c258828de9f3996c8999428ab29cfd01f5b86d8
williamg1750 <william.guan12@gmail.com>
2021-06-15 16:16:14 -0400

    bcrypt compare

--- index.js
+++ index.js
@@ -1,11 +1,23 @@
 const bcrypt = require('bcrypt');
 
 const hashedPassword = async (pw) => {
-  const salt = await bcrypt.genSalt(10);
+  const salt = await bcrypt.genSalt(12);
   const hash = await bcrypt.hash(pw, salt);
   console.log(`this is pw before hash ${pw}`);
   console.log(salt);
   console.log(hash);
 };
 
-hashedPassword('monkey');
+const login = async (pw, hashedPass) => {
+  const result = await bcrypt.compare(pw, hashedPass);
+  if (result) {
+    console.log('LOGGED IN with correct password');
+  } else {
+    console.log('INCORRECT PASSWORD');
+  }
+};
+
+// hashedPassword('monkey');
+
+// $2b$12$8ASxUnvyKBI00KUyyPhONeesbR5LTdh.nJLUL5qVfWcmWMb5y.ZKi;
+login('monkey', '$2b$12$8ASxUnvyKBI00KUyyPhONeesbR5LTdh.nJLUL5qVfWcmWMb5y.ZKi');
