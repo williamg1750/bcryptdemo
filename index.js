719615841bffc08726d1ae3bd4bf4a76b8a3607c
williamg1750 <william.guan12@gmail.com>
2021-06-16 13:01:13 -0400

    auth demo complete

--- index.js
+++ index.js
@@ -35,7 +35,7 @@ app.get('/register', (req, res) => {
 
 app.post('/register', async (req, res) => {
   const { username, password } = req.body;
-  const hash = await bcrypt.hash(password, 12);
+  // const hash = await bcrypt.hash(password, 12);
   const user = new User({
     username,
     password: hash,
@@ -51,10 +51,9 @@ app.get('/login', (req, res) => {
 
 app.post('/login', async (req, res) => {
   const { username, password } = req.body;
-  const user = await User.findOne({ username });
-  const validPassword = await bcrypt.compare(password, user.password);
-  if (validPassword) {
-    req.session.user_id = user._id;
+  const foundUser = await User.findAndValidate(username, password);
+  if (foundUser) {
+    req.session.user_id = foundUser._id;
     res.redirect('/secret');
   } else {
     res.redirect('/login');
--- models/user.js
+++ models/user.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
 
 const userSchema = new mongoose.Schema({
   username: {
@@ -9,6 +10,20 @@ const userSchema = new mongoose.Schema({
     type: String,
     required: [true, 'Password can NOT be blank'],
   },
+});
+
+//remember cant use arrow function beacuse the context of this is going to be messed up
+userSchema.statics.findAndValidate = async function (username, password) {
+  const foundUser = await this.findOne({ username });
+  const isValid = await bcrypt.compare(password, foundUser.password);
+  return isValid ? foundUser : false;
+};
+
+//this is a middleware hashes the password before saving
+userSchema.pre('save', async function (next) {
+  if (!this.isModified('password')) return next();
+  this.password = await bcrypt.hash(this.password, 12);
+  next();
 });
 
 module.exports = mongoose.model('User', userSchema);
